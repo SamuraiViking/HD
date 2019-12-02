ec63fd9ef73f253f16cddcec172d8bc4a6c9bc56
Kevin Nelson <nelson67@stolaf.edu>
2019-12-02 13:14:19 -0600

    assembly examples

---
+++ assembly/input_output/input_output.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    int x, y, z;
+    printf("Enter 3 numbers: ");
+    scanf("%d %d %d", &x, &y, &z);
+    printf("You entered the numbers: %d %d and %d\n", x, y, z);
+
+    return 0;
+}
