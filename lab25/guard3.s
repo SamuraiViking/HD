0fe301d06f51ca03e0fb06d38b7a011b22467041
Kevin Nelson <nelson67@stolaf.edu>
2019-12-03 22:37:36 -0600

    Lab24 B3 - guard x+y > 7

---
+++ lab25/guard3.s
@@ -0,0 +1,67 @@
+/* example of guard in assembly */
+
+/* format strings for I/O */    
+        .section .rodata
+        .align  2
+prompt: .asciz "Enter two integers x and y: "
+        .align  2
+scanf_fmt:
+        .asciz "%d %d"
+        .align  2
+not_gt_msg: .asciz "%d is not greater than %d\n"
+        .align  2
+gt_msg: .asciz "%d is greater than %d\n"
+
+/* main program */
+        .text
+        .align  2
+        .global main
+main:
+        push    {fp, lr}        @ setup stack frame
+        add     fp, sp, #4
+        sub     sp, sp, #8      @ #8 for 2 local variables
+        @ [fp, #-8] is x, first integer variable
+        @ [fp, #-12] is y, second integer variable
+
+        ldr     r0, promptP     @ print prompt
+        bl      printf
+        ldr     r0, scanf_fmtP  @ read values of x and y
+        sub     r1, fp, #12
+        sub     r2, fp, #8
+        bl      __isoc99_scanf
+
+        ldr     r0, [fp, #-12]   @ if y > x
+        ldr     r1, [fp, #-8]
+        add     r2, r0, r1
+        cmp     r2, #7          
+        bgt     greater_than_seven
+        ldr     r0, not_gt_msgP     @ print less-than/equal-to message
+        ldr     r1, [fp, #-12]   
+        ldr     r2, [fp, #-8]  
+        bl      printf
+        b       next
+
+greater_than_seven:
+        ldr     r0, gt_msgP     @ otherwise print greater-than message
+
+        str     r2, [fp, #-16]
+
+        ldr     r1, [fp, #-16]
+        ldr     r2, [fp, #-12] 
+        bl      printf
+
+next:
+        mov     r0, #0          @ assign 0 as return value
+        sub     sp, fp, #4      @ tear down stack frame
+        pop     {fp, pc}
+
+/* pointer variables for format strings */
+        .align  2
+promptP:
+        .word   prompt
+scanf_fmtP:
+        .word   scanf_fmt       
+not_gt_msgP:
+        .word   not_gt_msg
+gt_msgP:
+        .word   gt_msg
