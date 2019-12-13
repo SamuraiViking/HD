995b91ff4290d960af1e4dd44d742e1b6f0b36ac
Kevin Nelson <nelson67@stolaf.edu>
2019-12-13 07:00:01 +0000

    Update stage3.s

--- proj/stage3.s
+++ proj/stage3.s
@@ -7,75 +7,81 @@
 ///////////////
  */
 get_trans:
-    push    {fp, lr}
+    push    {fp, lr}                @ setup stackframe
     add     fp, sp, #0
     sub     sp, sp, #16
 
-    ldr     r0, enter_trans_charsP
+    ldr     r0, enter_trans_charsP  @ prompt user to input trans chars
     bl      printf
 
     ldr     r0, trans_charsP
     mov     r1, #100
-    bl      get_line                @ prompt user for input and hold input in input_chars
+    bl      get_line                @ get trans chars from user
 
-    mov     r1, #1
+    mov     r1, #1                  @ get input char
     ldr     r0, trans_charsP
-    bl      get_byte                @ r0 = input_charsP[1]
+    bl      get_byte        
 
-    cmp     r0, #' '
+    cmp     r0, #' '                @ input char does not equal 
     bne     invalidFormat
 
-    mov     r1, #3
+    mov     r1, #3                  @ get output char
     ldr     r0, trans_charsP
-    bl      get_byte                @ r0 = input_charsP[3]
+    bl      get_byte
 
-    cmp     r0, #'\n'
+    cmp     r0, #'\n'               @ output char does not equal new line
     bne     invalidFormat
 
     bl      validFormat
 
-invalidFormat:
+invalidFormat:                      @ return 0 and collapse stack frame
     mov     r0, #0
     sub     sp, fp, #0
     pop     {fp, pc}
 
-validFormat:
+validFormat:                        @ return 1 and collapse stack frame
     mov     r0, #1
     sub     sp, fp, #0
     pop     {fp, pc}
 
-is_letter:
+/* 
+Input: char 
+Output: 1 (true) or 0 (false)
+*/
+is_letter: 
     push    {fp, lr}
     add     fp, sp, #0
     sub     sp, sp, #10
 
+    @ [fp, #-8] is the argument char
+
     str     r0, [fp, #-8]
     ldr     r1, [fp, #-8]
 
-    cmp     r1, #'A'
+    cmp     r1, #'A'            @ not letter if r1 < A
     ble     not_letter
 
-    cmp     r1, #'z'
+    cmp     r1, #'z'            @ not letter if r1 > z
     bgt     not_letter
 
-    cmp     r1, #'Z'
+    cmp     r1, #'Z'            @ maybe letter if r1 > Z
     bgt     maybe_letter
 
-    bl      char_is_letter
+    bl      char_is_letter 
 
 maybe_letter:
-    cmp     r1, #'a'
+    cmp     r1, #'a'            @ not letter if r1 < a
     blt     not_letter
 
     bl      char_is_letter
 
 not_letter:
-    mov     r0, #0
+    mov     r0, #0              @ return 0
     sub     sp, fp, #0
     pop     {fp, pc}
 
 char_is_letter:
-    mov     r0, #1
+    mov     r0, #1              @ return 1
     sub     sp, fp, #0
     pop     {fp, pc}
 
@@ -95,52 +101,52 @@ translate:
     add     fp, sp, #0
     sub     sp, sp, #40
 
-    ldr     r0, trans_charsP
+    ldr     r0, trans_charsP    @ get inner char
     mov     r1, #0
     bl      get_byte
 
-    str     r0, [fp, #-8]
+    str     r0, [fp, #-8]       @ store inner char 
 
-    ldr     r0, trans_charsP
+    ldr     r0, trans_charsP    @ get outer char
     mov     r1, #2
     bl      get_byte
 
-    str     r0, [fp, #-12]
+    str     r0, [fp, #-12]      @ store outer char
 
-    mov     r1, #0
+    mov     r1, #0              @ init index as 0
     str     r1, [fp, #-16]
 
-    mov     r1, #1
+    mov     r1, #1              @ init between_words (boolean) as 1
     str     r1, [fp, #-20]
 
     bl      loop_through_chars
 
 translate_char:
-    ldr     r0, input_charsP
-    ldr     r1, [fp, #-16]
-    ldr     r2, [fp, #-12]
-    bl      put_byte
-    bl      next_char
+    ldr     r0, input_charsP    @ load input chars
+    ldr     r1, [fp, #-16]      @ load index
+    ldr     r2, [fp, #-12]      @ load out char
+    bl      put_byte            @ replace input char with output char
+    bl      next_char           
 
 not_between_words:
-    ldr     r1, [fp, #-20]
+    ldr     r1, [fp, #-20]      @ check if between_words
     cmp     r1, #1
     beq     count_word
     bl      translate_char_guard
 
 count_word:
-    add     r5, r5, #1
+    add     r5, r5, #1          @ add 1 to num of words
     mov     r1, #0
     str     r1, [fp, #-20]
     bl      translate_char_guard
 
 between_words:
-    mov     r1, #1
+    mov     r1, #1              @ between_words = true
     str     r1, [fp, #-20]
 
     bl      translate_char_guard
 
-next_char:
+next_char:                      @ increase index 
     ldr     r0, [fp, #-16]
     add     r0, r0, #1
     str     r0, [fp, #-16]
@@ -149,34 +155,34 @@ next_char:
     bl      loop_through_chars
 
 loop_through_chars:
-    ldr     r0, input_charsP
+    ldr     r0, input_charsP    @ get char of input char at index
     ldr     r1, [fp, #-16]
     bl      get_byte
 
-    str     r0, [fp, #-24]
+    str     r0, [fp, #-24]      @ store char
 
-    ldr     r0, [fp, #-24]
+    ldr     r0, [fp, #-24]      @ check if char equals space
     cmp     r0, #' '
     beq     between_words
 
-    ldr     r0, [fp, #-24]
+    ldr     r0, [fp, #-24]      @ check if char is letter
     bl      is_letter
 
-    cmp     r0, #1
+    cmp     r0, #1              @ if is letter then not_between_words
     beq     not_between_words
 
 translate_char_guard:
-    ldr     r0, [fp, #-24]
-    ldr     r1, [fp, #-8]
-    cmp     r0, r1
-    beq     translate_char
+    ldr     r0, [fp, #-24]      @ load current char
+    ldr     r1, [fp, #-8]       @ load inchar
+    cmp     r0, r1              @ check if current char equals inchar
+    beq     translate_char      @ translate inchar with outchar
 
-    ldr     r0, [fp, #-24]
+    ldr     r0, [fp, #-24]      @ load current char
 
-    cmp     r0, #0
-    bne     next_char
+    cmp     r0, #0              @ check if current char is 0
+    bne     next_char           @ if not 0 then go to next char
 
-    add     r4, r4, #1
+    add     r4, r4, #1          @ add 1 to num of chars
 
     sub     sp, fp, #0
     pop     {fp, pc}
@@ -187,26 +193,26 @@ translate_char_guard:
 //////////////////
 */
 print_summary:
-    push    {fp, lr}
+    push    {fp, lr}                @ set stack frame
     add     fp, sp, #0
     sub     sp, sp, #16
 
-    ldr     r0, summary_headerP
+    ldr     r0, summary_headerP     @ print header
     bl      printf
 
-    ldr     r0, num_of_charactersP
+    ldr     r0, num_of_charactersP  @ print num of chars
     mov     r1, r6
     bl      printf
 
-    ldr     r0, num_of_wordsP
+    ldr     r0, num_of_wordsP       @ print num of words
     mov     r1, r5
     bl      printf
 
-    ldr     r0, num_of_new_linesP
+    ldr     r0, num_of_new_linesP   @ print num of new lines
     mov     r1, r4
     bl      printf
 
-    sub     sp, fp, #0
+    sub     sp, fp, #0              @ colapse stack frame
     pop     {fp, pc}
 
 /*
@@ -215,17 +221,18 @@ print_summary:
 //////////
 */
 main:
-    push    {fp, lr}
+    push    {fp, lr}                @ setup stackframe 
     add     fp, sp, #0
     sub     sp, sp, #16
     
-    bl      get_trans  @ r0 == (input_chars[3] == '\n' && input_chars[1] == ' ') ? 1 : 0
+    bl      get_trans               @ get in char
+                                    @ get out char
 
     cmp     r0, #0
     beq     end_main
 
-    ldr     r0, enter_input_charsP
-    bl      printf
+    ldr     r0, enter_input_charsP  @ print input_chars
+    bl      printf                  
 
     mov     r4, #0      @ r4 = nLines
     mov     r5, #0      @ r5 = nWords
@@ -235,30 +242,30 @@ main:
 run_get_lines_again:
 
 get_lines:
-    ldr     r0, input_charsP
+    ldr     r0, input_charsP        @ get line from user
     mov     r1, #100
-    bl      get_line    @ get line from user
+    bl      get_line
 
-    ldr     r0, input_charsP
+    ldr     r0, input_charsP        @ get in char
     mov     r1, #0
-    bl      get_byte    @ r0 = input_chars[0]
+    bl      get_byte
 
-    cmp     r0, #'\n'
-    beq     end_get_lines    @ if r0[0] != '\n'
+    cmp     r0, #'\n'               @ end if line is new line
+    beq     end_get_lines
 
-    bl      translate
+    bl      translate               @ replace all inchars with outchars
 
-    ldr     r0, input_charsP
+    ldr     r0, input_charsP        @ print input chars
     bl      printf
 
-    bl      run_get_lines_again
+    bl      run_get_lines_again     @ get another line
 
 end_get_lines:
     bl      print_summary
     bl      end_main
 
-end_main:
-    mov     r0, #0
+end_main:   
+    mov     r0, #0                  @ colapse stack frame
     sub     sp, fp, #0
     pop     {fp, pc}
 
